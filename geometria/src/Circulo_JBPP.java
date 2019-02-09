d77d5b96858563c96d4d507b96f1e4fd2ec6c54d
Juan Bautista Penalva Perez <usu@usu-PC>
2019-02-09 14:37:57 +0100

    Siempre positivo

--- geometria/src/Circulo_JBPP.java
+++ geometria/src/Circulo_JBPP.java
@@ -6,6 +6,9 @@ public class Circulo_JBPP extends FiguraGeometrica_JBPP {
 	
 	public Circulo_JBPP(double r, String tipoFigura) {
 		super(tipoFigura);
+		if (r<0) {
+			r=r*-1;//convierte en positivo
+		}
 		radio = r;
 	}
 
